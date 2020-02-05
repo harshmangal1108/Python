8565178e6fe0e09e7645dd2fac65e8d582bc449c
harshmangal1108 <harsh.mangal1108@gmail.com>
2020-02-05 14:28:56 +0530

    Lab Work

--- Lab work/5feb.py
+++ Lab work/5feb.py
@@ -1,5 +1,5 @@
 ## list
-x=[1,2,3,4]
+x=[4,1,2,3,4]
 print(type(x))
 ### access item using index
 print(x[3])
@@ -18,3 +18,33 @@ if "HII" in x[4:]:
     print("yes")
 else:
     print("no")
+print("Enter value to search")
+#y=int(input())
+"""if y in x:
+    print("yes %d is present"%y)
+else :
+    print("no")"""
+#### length of list
+print(len(x))
+#### adding item in list
+x.append("harsh")
+print(x)
+x.remove(4) ### 4 is value
+print(x)
+x.pop(4) ### 4 is index
+del x[1]
+print(x)
+z=list(x)
+### copying a list
+y=list(x)
+print(y)
+### 2nd method
+y=x.copy()
+###join 2 list
+
+#### 2nd method
+for n in y:
+    x.append(y)
+print(x)
+#### sorting in list
+print(z.sort())
