a8935eeac8eefdce501ac4e9595c4a3a6c316111
harshmangal1108 <harsh.mangal1108@gmail.com>
2020-02-10 14:08:40 +0530

    loops

---
+++ Lab work/10feb1.py
@@ -0,0 +1,17 @@
+##exp 3(1)
+for i in range(1,10):
+    print("*"*i)
+##in file
+for x in "hello world":
+    print(x)
+## exp3(2)
+n=1
+while n<=5:
+    print(n)
+    n=n+1
+
+###in file
+x=int(input("enter number for which values to be printed"))
+while 0<=x:
+    print(x)
+    x=x-1
